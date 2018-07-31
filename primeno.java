94282ba0f38bd106be050634e0100dea1aff4271
vijayalakshmi121997 <41734864+vijayalakshmi121997@users.noreply.github.com>
2018-07-31 14:47:29 +0530

    Create primeno.java

---
+++ primeno.java
@@ -0,0 +1,24 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int low = 20, high = 50;
+ 
+        while (low < high) {
+            boolean flag = false;
+ 
+            for(int i = 2; i <= low/2; ++i) {
+                // condition for nonprime number
+                if(low % i == 0) {
+                    flag = true;
+                    break;
+                }
+            }
+ 
+            if (!flag)
+                System.out.print(low + " ");
+ 
+            ++low;
+        }
+	}
+}
