886cc8f84d0b9e3a0a9baa333a1b0ae2789dccf2
vijayalakshmi121997 <41734864+vijayalakshmi121997@users.noreply.github.com>
2018-07-31 14:12:56 +0530

    Create odd.java

---
+++ odd.java
@@ -0,0 +1,20 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int limit = 6;
+ 
+		System.out.println("Printing Odd numbers between 1 and " + limit);
+    for(int i=1; i <= limit; i++){
+    if( i % 2 != 0){
+				System.out.print(i + " ");
+			}
+		}
+	}
+}
+ 
+ 
+		
+ 
+ 
+	
